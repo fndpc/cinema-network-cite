92a3670bbd3065e3733cc4833a7e0eaa07fb6acc
unknown <cherryyt681@gmail.com>
2025-04-15 19:57:54 +0300

    fix navbar text

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.14.0a7-bookworm
+FROM python:3.13.3-slim
 
 ENV PYTHONUNBUFFERED=1
 
--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@t1qeg72a=-l60ss^mg339zvv&^x*9*&g*&oblv62m%_97(c-h'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
 INTERNAL_IPS = [
     # ...
     "127.0.0.1",
